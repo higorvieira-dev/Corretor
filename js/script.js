66e82fb23cacc3a612008cb5de5c7eea92fc043c
higorvieira-dev <higorvieira.dev@hotmail.com>
2023-10-12 18:46:23 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const openButton = document.querySelector("[data-open-modal]")
+const closeButton = document.querySelector("[data-close-modal]")
+const modal = document.querySelector("[data-modal]")
+
+openButton.addEventListener("click", () =>{
+    modal.showModal()
+})
+closeButton.addEventListener("click", () =>{
+    modal.close()
+})
+
